94f22db0de38697f98bb87f663751cb063d51123
Felix Lui <felixlui@MacBook-Pro.local>
2020-06-09 14:41:57 -0400

    Made an array of 6 colors and assigned the event listeners to each square to check if it is the picked color is the same as the one they are checking for.

--- rgbGuess.js
+++ rgbGuess.js
@@ -8,7 +8,31 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 
 for(var i = 0; i<squares.length; i++){
+	//Initial assignment of colors to the squares
 	squares[i].style.backgroundColor = colors[i];
+
+	//Adding in click listeners to allow for selection of squares
+	squares[i].addEventListener("click", function(){
+		console.log("clicked " + this.style.backgroundColor);
+		if (this.style.backgroundColor == pickedColor) {
+			console.log("Corrent");
+		}else{
+			console.log("Wrong");
+		}
+	})
 }
+
+// squares[i].addEventListener("click", function(){
+// 	if(this.backgroundColor == pickedColor){
+// 		document.getElementById("result").textContent = "Correct";
+// 	}else{
+// 		document.getElementById("result").textContent = "Incorrect";
+// 	}
+// })
