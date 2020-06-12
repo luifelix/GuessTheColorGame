d3a913bb20df5ad4ea73fdf06d01f2157b44cd9a
Felix Lui <felixlui@MacBook-Pro.local>
2020-06-12 16:33:55 -0400

    added the functionality of changing all the colors to the correct color when selected correctly. it will now also change the color of the background of h1 when guessed correctly

--- rgbGuess.js
+++ rgbGuess.js
@@ -1,18 +1,11 @@
-var colors = [
-	"rgb(255, 255, 255)",
-	"rgb(255, 0, 255)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(0, 0, 0)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[randomColor()];
+var computerPickedColor = colors[unifySquareColors()];
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 
-colorDisplay.textContent = pickedColor;
+colorDisplay.textContent = computerPickedColor;
 
 
 for(var i = 0; i<squares.length; i++){
@@ -21,11 +14,16 @@ for(var i = 0; i<squares.length; i++){
 
 	//Adding in click listeners to allow for selection of squares
 	squares[i].addEventListener("click", function(){
-		console.log("clicked " + this.style.backgroundColor);
-		if (this.style.backgroundColor == pickedColor) {
+		userPickedColor = this.style.backgroundColor
+		console.log("clicked " + userPickedColor);
+		console.log("userPickedColor: " + userPickedColor + 
+					" computerPickedColor" + computerPickedColor);
+		if (this.style.backgroundColor == computerPickedColor) {
 			console.log("Correct");
 			messageDisplay.textContent = "Well Done";
-			colorChange(pickedColor);
+			colorChange(computerPickedColor);
+			document.querySelector("h1").style.backgroundColor 
+					= computerPickedColor;
 		}else{
 			console.log("Wrong");
 			this.style.backgroundColor = "grey";
@@ -34,7 +32,22 @@ for(var i = 0; i<squares.length; i++){
 	})
 }
 
+function generateRandomColors(totalSquares){
+	var colorArray = [];
+	for(var i = 0; i < totalSquares; i++){
+		colorArray.push(randomColor());
+	}
+	return colorArray;
+}
+
 function randomColor(){
+	var red = Math.floor(Math.random()*256);
+	var green = Math.floor(Math.random()*256);
+	var blue = Math.floor(Math.random()*256);
+	return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+
+function unifySquareColors(){
 	return Math.floor(Math.random() * colors.length);
 }
 
